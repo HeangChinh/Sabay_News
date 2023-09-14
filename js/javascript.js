3c869f3c92ff321ce22a153fda63c4f9c50db344
Heang Chinh <heangchinh11@gmail.com>
2023-09-14 07:43:14 +0700

    update

---
+++ js/javascript.js
@@ -0,0 +1,11 @@
+let collape = document.querySelector('.navbar-collapse');
+let toggle = true;
+function navbar_toggle(){
+    if(toggle){
+        collape.style.height = '255px';
+        toggle = false;
+    }else{
+        collape.style.height = '0';
+        toggle = true;
+    }
+}
